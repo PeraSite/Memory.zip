b7262a1bcc2e9522a949ab9f8bba96aaa6cdbbc3
PeraSite <pretocki3@gmail.com>
2023-02-10 19:35:56 +0900

    feat: Inspector 설정 추가, 게임 오버 추가, 기본 체력 값 추가

--- Assets/MemoryZip/FlirtingGame/Scripts/FlirtingGameManager.cs
+++ Assets/MemoryZip/FlirtingGame/Scripts/FlirtingGameManager.cs
@@ -1,55 +1,109 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace MemoryZip.FlirtingGame {
-	public class FlirtingGameManager : MonoBehaviour {
+	public class FlirtingGameManager : SerializedMonoBehaviour {
+		[Header("설정"), SuffixLabel("초", Overlay = true)]
+		[SerializeField] private int _maxRound = 3;
+		[SerializeField] private float _defaultPercent = 30;
+
+		[SerializeField] private float _percentPerRival = 1;
+		[SerializeField] private float _percentPerSpace = 5;
+
 		[Header("UI")]
 		[SerializeField] private TextMeshProUGUI _currentRoundText;
-		[SerializeField] private TextMeshProUGUI _healthText;
-		[SerializeField] private Image _healthBar;
+		[SerializeField] private TextMeshProUGUI _completePercentText;
+		[SerializeField] private Image _completePercentBar;
 
-		private int _currentStage = 1;
-		private int _health = 100;
+		[Header("고양이 오브젝트")]
+		[SerializeField] private GameObject[] _maleCats;
+		[SerializeField] private GameObject[] _rivalCats;
 
-		public int CurrentStage {
-			get => _currentStage;
+		[Tooltip("현재 라운드 -> 보여질 수컷 고양이 오브젝트")]
+		[OdinSerialize] private Dictionary<int, GameObject> _maleCatDictionary = new();
+
+		[Tooltip("현재 라운드 -> 보여질 암컷 고양이 오브젝트 목록")]
+		[OdinSerialize] private Dictionary<int, List<GameObject>> _rivalCatDictionary = new();
+
+		private int _currentRound = 1;
+		private float _completePercent;
+		private int CurrentRound {
+			get => _currentRound;
 			set {
-				_currentStage = value;
+				_currentRound = value;
 				UpdateUI();
 			}
 		}
-		public int Health {
-			get => _health;
+		private float CompletePercent {
+			get => _completePercent;
 			set {
-				_health = value;
+				_completePercent = value;
 				UpdateUI();
+				CheckGameOver();
 			}
 		}
+		private float _timer;
 
 		private void Start() {
+			CompletePercent = _defaultPercent;
 			UpdateUI();
+			UpdateCats();
 		}
 
 		private void Update() {
+			HandleInput();
+			HandlePercentDecrease();
+		}
+		private void HandlePercentDecrease() {
+			CompletePercent = Mathf.Max(CompletePercent - _rivalCatDictionary[CurrentRound].Count * _percentPerRival * Time.deltaTime, 0);
+		}
+
+		private void HandleInput() {
 			if (Input.GetKeyDown(KeyCode.Space)) {
-				Health -= 5;
-				if (Health <= 0) {
-					NextStage();
+				// TODO: 공격 레이저 표시
+				CompletePercent += _percentPerSpace;
+				if (CompletePercent >= 100) {
+					NextRound();
 				}
 			}
 		}
 
-		private void NextStage() {
-			CurrentStage++;
-			Health = 100;
+		[Title("유틸")]
+		[Button]
+		private void NextRound() {
+			CurrentRound++;
+			if (CurrentRound > _maxRound) {
+				Debug.Log("Game Clear!");
+				return;
+			}
+			CompletePercent = _defaultPercent;
+			UpdateCats();
 		}
 
+		[ButtonGroup("UI")]
 		private void UpdateUI() {
-			_currentRoundText.text = $"{CurrentStage}라운드";
-			_healthText.text = $"{Health}";
-			_healthBar.fillAmount = (float)Health / 100;
+			_currentRoundText.text = $"{CurrentRound}라운드";
+			_completePercentText.text = $"{(int)CompletePercent}";
+			_completePercentBar.fillAmount = CompletePercent / 100;
+		}
+
+		[ButtonGroup("UI")]
+		private void UpdateCats() {
+			_maleCats.ForEach(x => x.SetActive(_maleCatDictionary[CurrentRound] == x));
+			_rivalCats.ForEach(x => x.SetActive(_rivalCatDictionary[CurrentRound].Contains(x)));
+		}
+
+		private void CheckGameOver() {
+			if (CompletePercent <= 0) {
+				Debug.Log("Game Over!");
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
 		}
 	}
 }
