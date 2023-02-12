ceff9427e065608c077267c447d97031b61c1156
PeraSite <pretocki3@gmail.com>
2023-02-12 22:48:44 +0900

    feat(눈빛보내기): 수컷 고양이 피격 애니메이션 추가, 게임 오버 효과 추가

--- Assets/MemoryZip/FlirtingGame/Scripts/FlirtingGameManager.cs
+++ Assets/MemoryZip/FlirtingGame/Scripts/FlirtingGameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using Sirenix.Utilities;
@@ -15,15 +16,21 @@ namespace MemoryZip.FlirtingGame {
 
 		[SerializeField] private float _percentPerRival = 1;
 		[SerializeField] private float _percentPerSpace = 5;
+		[SerializeField] private float _gameOverDelay = 5f;
 
 		[Header("UI")]
 		[SerializeField] private TextMeshProUGUI _currentRoundText;
 		[SerializeField] private TextMeshProUGUI _completePercentText;
 		[SerializeField] private Image _completePercentBar;
+		[SerializeField] private GameObject _gameOverObject;
+
 
 		[Header("주인공 애니메이션")]
 		[SerializeField] private Animator _playerAnimator;
 		[SerializeField] private GameObject _playerAttackAnimation;
+
+		[Header("수컷 고양이 스프라이트")]
+		[SerializeField] private Sprite[] _damagedMaleCatSprites;
 
 		[Header("고양이 오브젝트")]
 		[SerializeField] private GameObject[] _maleCats;
@@ -35,6 +42,7 @@ namespace MemoryZip.FlirtingGame {
 		[Tooltip("현재 라운드 -> 보여질 암컷 고양이 오브젝트 목록")]
 		[OdinSerialize] private Dictionary<int, List<GameObject>> _rivalCatDictionary = new();
 
+		private bool _isGameOver;
 		private int _currentRound = 1;
 		private float _completePercent;
 		private int CurrentRound {
@@ -52,8 +60,11 @@ namespace MemoryZip.FlirtingGame {
 				CheckGameOver();
 			}
 		}
+		private Sprite _maleCatOriginalSprite;
+
 		private float _timer;
 		private static readonly int IsAttack = Animator.StringToHash("IsAttack");
+		private static readonly int IsDead = Animator.StringToHash("IsDead");
 
 		private void Start() {
 			CompletePercent = _defaultPercent;
@@ -62,6 +73,7 @@ namespace MemoryZip.FlirtingGame {
 		}
 
 		private void Update() {
+			if (_isGameOver) return;
 			HandleAttackInput();
 			HandlePercentDecrease();
 		}
@@ -69,11 +81,24 @@ namespace MemoryZip.FlirtingGame {
 		private void HandleAttackInput() {
 			if (Input.GetKeyDown(KeyCode.Space)) {
 				// TODO: 공격 레이저 표시
+
+				// 함락 게이지 상승
 				CompletePercent += _percentPerSpace;
+
+				// 피격 애니메이션 표기
+				_maleCatDictionary[CurrentRound].GetComponent<SpriteRenderer>().sprite = _damagedMaleCatSprites[Random.Range(0, _damagedMaleCatSprites.Length)];
+				
+				// 다음 스테이지 체크
 				if (CompletePercent >= 100) {
 					NextRound();
 				}
 			}
+
+			if (Input.GetKeyUp(KeyCode.Space)) {
+				// 피격 애니메이션 초기화
+				_maleCatDictionary[CurrentRound].GetComponent<SpriteRenderer>().sprite = _maleCatOriginalSprite;
+			}
+
 
 			var isAttacking = Input.GetKey(KeyCode.Space);
 			_playerAnimator.SetBool(IsAttack, isAttacking);
@@ -89,6 +114,7 @@ namespace MemoryZip.FlirtingGame {
 		private void NextRound() {
 			CurrentRound++;
 			if (CurrentRound > _maxRound) {
+				_isGameOver = true;
 				Debug.Log("Game Clear!");
 				return;
 			}
@@ -105,15 +131,26 @@ namespace MemoryZip.FlirtingGame {
 
 		[ButtonGroup("UI")]
 		private void UpdateCats() {
+			_maleCatOriginalSprite = _maleCatDictionary[CurrentRound].GetComponent<SpriteRenderer>().sprite;
 			_maleCats.ForEach(x => x.SetActive(_maleCatDictionary[CurrentRound] == x));
 			_rivalCats.ForEach(x => x.SetActive(_rivalCatDictionary[CurrentRound].Contains(x)));
 		}
 
 		private void CheckGameOver() {
 			if (CompletePercent <= 0) {
-				Debug.Log("Game Over!");
-				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+				_isGameOver = true;
+				StartCoroutine(GameOver());
 			}
+		}
+
+		private IEnumerator GameOver() {
+			_playerAnimator.SetBool(IsDead, true);
+			_gameOverObject.SetActive(true);
+			_maleCats.ForEach(x => x.SetActive(false));
+			_rivalCats.ForEach(x => x.SetActive(false));
+
+			yield return new WaitForSeconds(_gameOverDelay);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		}
 	}
 }
