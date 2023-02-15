06a685bb77f3548b3427f2de7dbf0e3a08e5fe99
PeraSite <pretocki3@gmail.com>
2023-02-15 14:18:26 +0900

    feat: 슈라면 기본 이동, 맵, 씬 구조 제작

---
+++ Assets/MemoryZip/RamyeonGame/Scripts/RamyeonGameCharacter.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace MemoryZip.RamyeonGame {
+	public class RamyeonGameCharacter : MonoBehaviour {
+		[Header("¼³Á¤")]
+		[SerializeField] private float _moveSpeed;
+
+		private Animator _animator;
+		private Rigidbody2D _rigidbody;
+		private Vector2 _input;
+		private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
+		private void Awake() {
+			_animator = GetComponent<Animator>();
+			_rigidbody = GetComponent<Rigidbody2D>();
+		}
+
+		private void Update() {
+			ReadInput();
+		}
+		private void ReadInput() {
+			_input.x = Input.GetAxisRaw("Horizontal");
+			_input.y = Input.GetAxisRaw("Vertical");
+		}
+
+		private void FixedUpdate() {
+			Move();
+		}
+		private void Move() {
+			_rigidbody.velocity = _input.normalized * _moveSpeed;
+			_animator.SetBool(IsMoving, _input.magnitude > 0);
+		}
+	}
+}
